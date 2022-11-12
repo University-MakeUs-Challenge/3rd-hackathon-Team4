aacbb0330c0e55c3afdc704e6fab22396a2c0d14
이어진 <leeeojin6@gmail.com>
2022-11-13 07:14:21 +0900

    club

---
+++ src/main/java/com/team/health/domain/Reservation.java
@@ -0,0 +1,11 @@
+package com.team.health.domain;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Reservation {
+
+    private int reservationId;
+    private String isAllowed;
+
+}
