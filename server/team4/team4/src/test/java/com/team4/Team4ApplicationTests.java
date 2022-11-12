2995aa70f23e35f1b171989f960aa029eca67450
mingsound21 <mingsound@kakao.com>
2022-11-12 22:13:35 +0900

    feat : server project 생성

---
+++ server/team4/team4/src/test/java/com/team4/Team4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
