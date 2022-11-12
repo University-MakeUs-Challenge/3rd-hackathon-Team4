d39c00a0a0075de7504352eab5b972016e02af68
cyjadela <adelacho@naver.com>
2022-11-12 22:40:21 +0900

    Feat: 하단바 구현

---
+++ client/app/src/main/java/com/example/client/MypageFragment.kt
@@ -0,0 +1,23 @@
+package com.example.client
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.client.databinding.FragmentMeetingBinding
+import com.example.client.databinding.FragmentMypageBinding
+
+class MypageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+    return FragmentMypageBinding.inflate(layoutInflater).root
+
+    }
+
+}
