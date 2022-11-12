2ab1d26199cb1326dc1068b7d6ecd51b71e4c7f8
parkyejin <uiop5809@naver.com>
2022-11-13 05:57:47 +0900

    Feat: 온보딩 구현

--- client/app/src/main/java/com/example/client/SubtitleActivity.kt
+++ client/app/src/main/java/com/example/client/SubtitleActivity.kt
@@ -1,4 +1,20 @@
 package com.example.client
 
-class SubTitleActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.client.databinding.ActivitySubtitleBinding
+
+class SubtitleActivity:AppCompatActivity() {
+    private lateinit var binding:ActivitySubtitleBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySubtitleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+           var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
