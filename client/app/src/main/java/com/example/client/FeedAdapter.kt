c26a46870ac993c7f150f7229c9b08d194b16a97
alex-koko <bookshelf1234@ewhain.net>
2022-11-13 04:09:25 +0900

    Feat: 피드 UI 구현

---
+++ client/app/src/main/java/com/example/client/FeedAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.client
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.client.databinding.ViewItemLayoutBinding
+
+class FeedAdapter(private val feedList: ArrayList<FeedData>): RecyclerView.Adapter<FeedAdapter.ViewHolder>() {
+
+    override fun getItemCount(): Int {
+        return feedList.size
+    }
+
+    override fun onBindViewHolder(holder: FeedAdapter.ViewHolder, position: Int) {
+       // holder.bind(feedList[position])
+        holder.itemView.setOnClickListener {
+            mItemClickListener.onItemClick(feedList[position])
+        }
+    }
+    interface MyItemClickListener{
+        fun onItemClick(feed: FeedData)
+    }
+
+    private lateinit var mItemClickListener: MyItemClickListener
+
+    fun setMyItemClickListener(itemClickListener: MyItemClickListener){
+        mItemClickListener = itemClickListener
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): FeedAdapter.ViewHolder {
+        val binding: ViewItemLayoutBinding = ViewItemLayoutBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        return ViewHolder(binding)
+    }
+
+
+    inner class ViewHolder(val binding: ViewItemLayoutBinding): RecyclerView.ViewHolder(binding.root){
+
+//        fun bind(feed: FeedData){
+//            Glide.with(binding.postImgView)feed.img
+//        }
+    }
+
+}
---
+++ client/app/src/main/java/com/example/client/FeedData.kt
@@ -0,0 +1,5 @@
+package com.example.client
+
+data class FeedData (
+    val img : Int
+    )
