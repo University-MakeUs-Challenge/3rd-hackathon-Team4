06c02819258340d28d01d76ac9c8a5a13232ffc4
MingyeomKim <mingyum119@naver.com>
2022-11-13 02:38:27 +0900

    User 모델 생성 완료

--- src/main/java/com/team/health/domain/User.java
+++ src/main/java/com/team/health/domain/User.java
@@ -2,13 +2,27 @@ package com.team.health.domain;
 
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+
+import java.time.LocalDateTime;
 
 @Getter
 public class User {
     @Id
+    @Column("user_id")
     private Integer userId;
 
     private String name;
 
     private Integer age;
+
+    private Rank rank = Rank.BRONZE;
+
+    private String phoneNumber;
+
+    private String email;
+
+    private LocalDateTime createdAt;
+
+    private Integer universityId;
 }
