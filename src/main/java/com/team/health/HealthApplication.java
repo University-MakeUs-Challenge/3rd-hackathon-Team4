05eabf026cf8d92ed14a5ab1a1ae629d267fcf74
MingyeomKim <mingyum119@naver.com>
2022-11-12 22:45:55 +0900

    Feat: server branch 추가

---
+++ src/main/java/com/team/health/HealthApplication.java
@@ -0,0 +1,13 @@
+package com.team.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthApplication.class, args);
+	}
+
+}
