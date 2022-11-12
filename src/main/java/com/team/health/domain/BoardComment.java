8408e737d1452ee6fc9abdda2465b0322ecf86bb
mingsound21 <mingsound@kakao.com>
2022-11-13 06:26:41 +0900

    Feat: BoardComment 도메인 생성

--- src/main/java/com/team/health/domain/BoardComment.java
+++ src/main/java/com/team/health/domain/BoardComment.java
@@ -1,2 +1,19 @@
-package com.team.health.domain;public class BoardComment {
+package com.team.health.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class BoardComment {
+    int commentId;
+    String content;
+    Integer parentCommentId;
+    Integer sequence;
+    LocalDateTime creaetedAt;
+    LocalDateTime updatedAt;
 }
